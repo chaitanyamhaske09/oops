8359c20c3a550245abc09065b865d371e8d20ba0
KASUTUBH <kaustubhwakchoure@gmail.com>
2024-12-10 23:03:51 +0530

    opp exp 1.cpp

--- exp 1.cpp
+++ exp 1.cpp
@@ -1,4 +1,9 @@
-
+Implement a class Complex which represents the Complex number data type. Implement the
+following:
+    1. Constructor (including a default constructor which creates the complex number 0 + 0i)
+    2. Overload operator + to add two complex numbers
+    3. Overload operator * to multiply two complex numbers
+    4. Overload Operators << and >> to print and read Complex numbers
 
 #include<iostream>
 using namespace std;
